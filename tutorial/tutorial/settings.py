30e9d577a3c4d3b3d43a9b05bd3fb3df043ab81d
Jumpei <jumpyoshim@gmail.com>
2018-07-02 15:07:37 +0900

    Use Serializer.

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
